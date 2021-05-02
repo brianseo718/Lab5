1991f3b459ac12f8055979ee6d4977d778d63a24
Brian Seo <bkseo@ucsd.edu>
2021-05-02 13:06:43 -0700

    finished part1

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # CSE110 SP21 Lab 5
 
 ## Author(s):
-- YOUR NAME
+- Brian Seo
+- Yunhao Du
 
 ## Part 1:
 
--- script.js
+++ script.js
@@ -1,17 +1,102 @@
 // script.js
-
 const img = new Image(); // used to load image from <input> and draw to canvas
-
+const canvas = document.getElementById("user-image");
+const ctx = canvas.getContext('2d');
+//img.src = '//:0';
 // Fires whenever the img object loads a new image (such as with img.src =)
+const input_image = document.getElementById("image-input");
+const sumbit_in = document.getElementById("generate-meme");
+const clear = document.querySelector('button[type=reset]');
+const read_text = document.querySelector('button[type=button]');
+const top_text = document.getElementById("text-top");
+const bottom_text = document.getElementById("text-bottom");
+var synth = window.speechSynthesis;
+var voiceSel = document.getElementById("voice-selection");
+var voiceArr = [];
+document.getElementById("voice-selection").disabled = false;
+const volume_slider = document.getElementById('volume-group');
+const volume_level = document.querySelector("[type='range']");
+const Img = document.querySelector('img');
+const Input =document.querySelector('input');
 img.addEventListener('load', () => {
-  // TODO
-
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  document.querySelector("[type='submit']").disabled = false;
+  document.querySelector("[type='reset']").disabled = true;
+  document.querySelector("[type='button']").disabled = true;
+  document.getElementById("voice-selection").disabled = false;
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  var image_in = getDimmensions(canvas.width,canvas.height,img.width,img.height);
+  ctx.drawImage(img,image_in.startX,image_in.startY,image_in.width,image_in.height);
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
 });
-
+function populateVoiceList(){
+  voiceArr = synth.getVoices();
+  voiceSel.removeChild(voiceSel.children[0]);
+  for(var i = 0;i  < voiceArr.length; i++){
+    var option = document.createElement('option');
+    option.textContent = voiceArr[i].name + ' (' + voiceArr[i].lang + ')';
+    if(voiceArr[i].default){
+      option.textContent += ' -- DEFAULT';
+    }
+    option.setAttribute('data-lang',voiceArr[i].lang);
+    option.setAttribute('data-name',voiceArr[i].name);
+    voiceSel.appendChild(option);
+  }
+}
+input_image.addEventListener('change', () => {
+  img.src = window.URL.createObjectURL(input_image.files[0]);
+  img.alt = input_image.files[0].name;
+});
+sumbit_in.addEventListener('submit',function(event) {
+  event.preventDefault();
+  populateVoiceList();
+  ctx.font = "40px Arial";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText(top_text.value, canvas.width/2,40);
+  ctx.fillText(bottom_text.value, canvas.width/2,canvas.height-20);
+  document.querySelector("[type='submit']").disabled = true;
+  document.querySelector("[type='reset']").disabled = false;
+  document.querySelector("[type='button']").disabled = false;
+});
+clear.addEventListener('click',()=>{
+  top_text.value="";
+  bottom_text.value="";
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  document.querySelector("[type='submit']").disabled = false;
+  document.querySelector("[type='reset']").disabled = true;
+  document.querySelector("[type='button']").disabled = true;
+});
+read_text.addEventListener('click',() =>{
+  const utterance = new SpeechSynthesisUtterance( top_text.value + "" + bottom_text.value);
+  utterance.volume= volume_level.value / 100;
+  const speechOption = window.speechSynthesis.getVoices();
+  const langOption = speechOption.find((voice) => voiceSel.selectedOptions[0].getAttribute('data-name') === voice.name);
+  utterance.voice = langOption;
+  speechSynthesis.speak(utterance);
+});
+volume_slider.addEventListener('input',() =>{
+  if(volume_level.value >= 67 && volume_level.value <= 100){
+    Img.src = "icons/volume-level-3.svg";
+    Img.alt = "Volume Level 3";
+  }
+  else if(volume_level.value >= 34 && volume_level.value <= 66){
+    Img.src = "icons/volume-level-2.svg";
+    Img.alt = "Volume Level 2";
+  }
+  else if(volume_level.value >= 1 && volume_level.value <= 33){
+    Img.src = "icons/volume-level-1.svg";
+    Img.alt = "Volume Level 1";
+  }
+  else{
+    Img.src = "icons/volume-level-0.svg";
+    Img.alt = "Volume Level 0";
+  }
+});
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
  * dimensions of the image so that it fits perfectly into the Canvas and maintains aspect ratio
@@ -25,10 +110,8 @@ img.addEventListener('load', () => {
  */
 function getDimmensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
   let aspectRatio, height, width, startX, startY;
-
   // Get the aspect ratio, used so the picture always fits inside the canvas
   aspectRatio = imageWidth / imageHeight;
-
   // If the apsect ratio is less than 1 it's a verical image
   if (aspectRatio < 1) {
     // Height is the max possible given the canvas
@@ -48,6 +131,5 @@ function getDimmensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
     startX = 0;
     startY = (canvasHeight - height) / 2;
   }
-
   return { 'width': width, 'height': height, 'startX': startX, 'startY': startY }
 }
